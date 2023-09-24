53975a0440cce6aa1c6583c2702388be29c7350f
Fred Robins <fredrobins619@outlook.com>
2023-09-24 08:28:41 +0000

    Update file structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className='bg-neutral-400'>Hi</p>
+      </div>
+    </>
+  )
+}
+
+export default App
