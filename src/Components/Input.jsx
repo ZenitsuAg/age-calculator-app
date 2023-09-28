280b8d048fc47ff0be779d07eb01765b2a35777a
Fred Robins <fredrobins619@outlook.com>
2023-09-28 21:06:18 +0000

    Update Input.jsx

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,8 +1,8 @@
 export default function Input({ value, onChange }) {
     return (
         <div className="mb-4 tracking-widest uppercase">
-            <label htmlFor={value}>{value}</label> {/* Check how to convert the first letter of a string to uppercase */}
-            <input type="number" name={value} id={value} onChange={onChange} className="shadow-inner"/>
+            <label htmlFor={value.toLowerCase()}>{value}</label>
+            <input type="number" name={value} id={value} onChange={onChange} className="p-2 mt-2 bg-gray-100 appearance-none"/>
         </div>
     )
 }
