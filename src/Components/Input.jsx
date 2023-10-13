f0ee24991865ad26658134a133b3dacbf371039c
Fred Robins <fredrobins619@outlook.com>
2023-10-13 21:28:54 +0000

    Change file structure and update conditionals

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,10 +1,20 @@
-export default function Input({ value, onChange, placeholder, error1 = false, error2 , errorMsg }) {
+export default function Input({
+    value,
+    onChange,
+    placeholder,
+    error1 = false,
+    error2 = false,
+    errorMsg,
+    formSubmitted = false,
+}) {
     return (
         <div>
             <div className="tracking-widest uppercase">
                 <label
                     htmlFor={value.toLowerCase()}
-                    className="text-[0.7rem] font-bold text-smokey-grey"
+                    className={`text-[0.7rem] font-bold text-smokey-grey ${
+                        formSubmitted && (error1 || error2) && "text-red-600"
+                    }`}
                 >
                     {value}
                 </label>
@@ -14,18 +24,22 @@ export default function Input({ value, onChange, placeholder, error1 = false, er
                     id={value}
                     onChange={onChange}
                     placeholder={placeholder}
-                    className={`w-full p-2 mt-2 text-2xl border rounded-lg outline-none border-off-white focus:border-off-black focus:border-1 ${error1 && 'border-light-red'} ${error2 && 'border-light-red'}`}
+                    className={`w-full p-2 mt-2 text-2xl border rounded-lg outline-none border-off-white focus:border-off-black focus:border-1 ${
+                        formSubmitted &&
+                        (error1 || error2) &&
+                        "border-light-red"
+                    }`}
                 />
             </div>
-            {error1 && (
+            {formSubmitted && error1 && (
                 <p className="text-[0.65rem] italic text-light-red transform-none">
                     This field is required
                 </p>
             )}
-            {error2 && (
+            {formSubmitted && error2 && (
                 <p className="text-[0.65rem] italic text-light-red transform-none">
-                {errorMsg}
-            </p>
+                    {errorMsg}
+                </p>
             )}
         </div>
     );
