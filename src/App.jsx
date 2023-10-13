35123f728fa9e5a10621664ef00a51d35b1dc666
Fred Robins <fredrobins619@outlook.com>
2023-10-13 21:09:36 +0000

    Improve code structure and lift state up

--- src/App.jsx
+++ src/App.jsx
@@ -6,61 +6,69 @@ function App() {
     const [months, setMonths] = useState(0);
     const [days, setDays] = useState(0);
 
-    function handleYears(e) {
-        setYears(e.target.value);
-    }
+    const [yr, setYr] = useState("--");
+    const [mnth, setMnth] = useState("--");
+    const [dy, setDy] = useState("--");
 
-    function handleMonths(e) {
-        setMonths(e.target.value);
-    }
-
-    function handleDays(e) {
-        setDays(e.target.value);
-    }
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     function handleSubmit(e) {
-        // e.preventDefault()
+        e.preventDefault()
 
-        alert("Fomr is submitted")
+        // Get the current date
+        const currentDate = new Date();
+
+        // Get the target date
+        const targetDate = new Date(years, months - 1, days);
+
+        // The time difference in milliseconds
+        const timeDifference = currentDate - targetDate;
+
+        const yrValue = Math.round(timeDifference / 3.154e10);
+        const mnthValue = Math.round((timeDifference % 3.154e10) / 2.628e9);
+        const dyValue = Math.round(((timeDifference % 3.154e10) % 2.628e9) / 8.64e7);
+
+        // Lift the values up, so that we can share it with the h1 element that needs it.
+        setYr(yrValue);
+        setMnth(mnthValue);
+        setDy(dyValue);
+
+        setFormSubmitted(true);
     }
 
-    const timeDifference =
-        Date.now() -
-        Date.parse(new Date(`${years}, ${months}, ${days}`)).toString();
-    const yr = Math.round(timeDifference / 3.154e10);
-    const mnth = Math.round((timeDifference % 3.154e10) / 2.628e9);
-    const dy = Math.round(((timeDifference % 3.154e10) % 2.628e9) / 8.64e7);
-
     return (
-        <main className="h-screen p-3 bg-off-white">
-            <div className="font-Poppins max-w-[375px] mx-auto bg-white rounded-2xl rounded-br-[100px] p-6 pt-11 mt-20 shadow-xl shadow-purple/10 md:max-w-[650px] md:p-10">
-                <form action="">
+        <main className="h-screen p-3 bg-off-white md:flex md:items-center md:justify-center">
+            <div className="font-Poppins max-w-[375px] mx-auto bg-white rounded-2xl rounded-br-[100px] p-6 pt-11 mt-20 md:mt-0 shadow-xl shadow-purple/10 md:min-w-[650px] md:p-10">
+                <form action="" onSubmit={handleSubmit}>
                     <div className="flex flex-row max-w-[300px] gap-3 mx-auto md:mx-0 md:max-w-[390px]">
                         <Input
                             value={"Day"}
-                            onChange={handleDays}
+                            onChange={(e) => setDays(e.target.value)}
                             placeholder={"DD"}
                             error1={days == ""}
                             error2={days > 31 ? true : false}
                             errorMsg={"Must be a valid day"}
+                            formSubmitted={formSubmitted}
                         />
                         <Input
                             value={"Month"}
-                            onChange={handleMonths}
+                            onChange={(e) => setMonths(e.target.value)}
                             placeholder={"MM"}
                             error1={months == ""}
-                            error2={months > 11 ? true : false}
+                            error2={(months > 12 || months < 1) ? true : false}
                             errorMsg={"Must be a valid month"}
+                            formSubmitted={formSubmitted}
                         />
                         <Input
                             value={"Year"}
-                            onChange={handleYears}
+                            onChange={(e) => setYears(e.target.value)}
                             placeholder={"YYYY"}
                             error1={years == ""}
                             error2={
                                 years > new Date().getFullYear() ? true : false
                             }
                             errorMsg={"Must be in the past!"}
+                            formSubmitted={formSubmitted}
                         />
                     </div>
 
@@ -71,22 +79,23 @@ function App() {
                         <div className="absolute top-0 left-0 right-0 text-center md:left-auto md:text-left">
                             <button
                                 type="submit"
-                                onSubmit={alert("hi")}
                                 className="w-12 h-12 p-8 mx-auto bg-center bg-no-repeat rounded-full md:h-16 md:w-16 bg-purple bg-arrow-icon hover:bg-off-black"
                             ></button>
                         </div>
                     </div>
                 </form>
 
-                <h1 className="text-5xl italic font-bold leading-tight md:text-6xl">
-                    <span className="text-purple">{yr}</span> years
-                </h1>
-                <h1 className="text-5xl italic font-bold leading-tight md:text-6xl">
-                    <span className="text-purple">{mnth}</span> months
-                </h1>
-                <h1 className="text-5xl italic font-bold leading-tight md:text-6xl">
-                    <span className="text-purple">{dy}</span> days
-                </h1>
+               <div className="text-5xl italic font-bold leading-tight md:text-6xl">
+                    <h1 className="">
+                        <span className="text-purple">{yr}</span> years
+                    </h1>
+                    <h1>
+                        <span className="text-purple">{mnth}</span> months
+                    </h1>
+                    <h1>
+                        <span className="text-purple">{dy}</span> days
+                    </h1>
+               </div>
             </div>
         </main>
     );
